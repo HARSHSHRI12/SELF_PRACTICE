dd37f94ca2cb08596b88d3266fa7147af09df57c
Harsh Shrivastava <141989341+HARSHSHRI12@users.noreply.github.com>
2024-04-07 01:03:17 +0530

    Update negative_pos.cpp

--- negative_pos.cpp
+++ negative_pos.cpp
@@ -1,26 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int arr[10],n;
-    cout<<"Enter size of array :";
-    cin>>n;
-    cout<<"Enter element :";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int negative;
-    int positive;
-for(int i=0;i<n;i++){
-    if(arr[i]<0){
-        negative=arr[i];
-    }
-    if(arr[i]>0){
-        positive=arr[i];
-    }
-}
 
-cout<<"Negative number in array is :"<<negative<<endl;
-cout<<"Positive number in array  is :"<<positive<<endl;
-return 0;
+int main() {
+    int arr[10], n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
+    int negatives[10], positives[10];
+    int negCount = 0, posCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            negatives[negCount] = arr[i];
+            negCount++;
+        }
+        if (arr[i] > 0) {
+            positives[posCount] = arr[i];
+            posCount++;
+        }
+    }
+
+    cout << "Negative numbers in array are: ";
+    for (int i = 0; i < negCount; i++) {
+        cout << negatives[i] << " ";
+    }
+    
+    cout << "\nPositive numbers in array are: ";
+    for (int i = 0; i < posCount; i++) {
+        cout << positives[i] << " ";
+    }
+
+    return 0;
 }
